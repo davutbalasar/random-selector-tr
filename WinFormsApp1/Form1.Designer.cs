a0f443204af4a04375cc146e38b1fbf69927cbb1
davut <davut.balasar@hotmail.com>
2026-07-07 20:02:51 +0300

    Initial commit: Added Windows Forms .NET 8.0 source code

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(20, 20);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(234, 27);
+            textBox1.TabIndex = 0;
+            textBox1.KeyDown += textBox1_KeyDown;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(260, 20);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 27);
+            button1.TabIndex = 1;
+            button1.Text = "Ekle";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(260, 53);
+            button2.Name = "button2";
+            button2.Size = new Size(100, 27);
+            button2.TabIndex = 2;
+            button2.Text = "Sil";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(20, 303);
+            button3.Name = "button3";
+            button3.Size = new Size(100, 27);
+            button3.TabIndex = 3;
+            button3.Text = "Seç";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(126, 306);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 20);
+            label1.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(20, 53);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(234, 244);
+            listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(382, 353);
+            Controls.Add(listBox1);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Rastgele Seçici";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private ListBox listBox1;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        private string filePath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            "RastgeleSecici",
+            "ogeler.txt"
+        );
+
+        public Form1()
+        {
+        InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(filePath))
+            {
+                string[] savedItems = File.ReadAllLines(filePath);
+                listBox1.Items.AddRange(savedItems);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                listBox1.Items.Add(textBox1.Text);
+                textBox1.Clear();
+                SaveItems();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                SaveItems();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                Random rnd = new Random();
+                int index = rnd.Next(listBox1.Items.Count);
+                label1.Text = "Seçilen: " + listBox1.Items[index].ToString();
+            }
+            else
+            {
+                label1.Text = "Seçilecek hiçbir şey yok!";
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(sender, e);
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void SaveItems()
+        {
+            string folder = Path.GetDirectoryName(filePath);
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+            File.WriteAllLines(filePath, listBox1.Items.Cast<string>());
+        }
+    }
+}
